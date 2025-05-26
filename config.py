d8068b3d260c4373cf5ac89075c94afe3f9d7648
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2025-05-26 23:31:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ load_dotenv()
 API_ID = int(getenv("API_ID","23803580"))
 API_HASH = getenv("API_HASH","7d91da02949db09dc81df55532c93863")
 # ------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN","7768882721:AAGrfDs0m0y8jMDBMkm9kdE9U-dtCCrphtY")
+BOT_TOKEN = getenv("BOT_TOKEN","7768882721:AAFejSaC0miWBLt6aJqwi-C8GEJWUT2ZFuM")
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","NoMoreMaxim")
 # --------------------------------------------------------
